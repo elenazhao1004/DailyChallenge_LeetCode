6a8cea17e835d3eb7b19b15f94ba016d87fd1664
Elena Zhao <56453745+elenazhao1004@users.noreply.github.com>
2020-11-24 12:27:18 -0800

    Add files via upload

---
+++ 1312.让字符串成为回文串的最少插入次数.java
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=1312 lang=java
+ *
+ * [1312] 让字符串成为回文串的最少插入次数
+ */
+
+// @lc code=start
+class Solution {
+    public int minInsertions(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    if (i + 1 == j) {
+                        dp[i][j] = 0;
+                    } else {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                } else {
+                    dp[i][j] = Math.min(dp[i + 1][j], dp[i][j - 1]) + 1;
+                }
+            }
+        }
+        return dp[0][n - 1];
+
+    }
+}
+
+// @lc code=end
+
