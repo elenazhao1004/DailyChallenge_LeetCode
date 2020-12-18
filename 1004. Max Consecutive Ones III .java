ace6ec94e70dab7aa3f9dcfa50a7ea7a1ea4af6e
Elena Zhao <56453745+elenazhao1004@users.noreply.github.com>
2020-12-17 22:49:46 -0800

    Add files via upload

--- 1004. Max Consecutive Ones III .java
+++ 1004. Max Consecutive Ones III .java
@@ -2,16 +2,18 @@
 
 // @lc code=start
 class Solution {
-    public int longestOnes(int[] A, int K) {
-        int left = 0,count = 0, i;
-        //i即窗口的right
-        for(i = 0; i < A.length;i++){
-            if(A[i] == 0) count++;
-            if(count > K){
-                if(A[left++] == 0) count--;
+    public int numberOfSubstrings(String s) {
+        int[] count = new int[]{0, 0, 0};
+        int left = 0, res = 0, n = s.length();
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < n; i++) {
+            count[arr[i] - 'a']++;
+            while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
+                res += n - i; //找到符合条件的子串时，它右边扩展出来的子串也是符合要求的
+                count[arr[left++] - 'a']--;
             }
         }
-        return i - left;
+        return res;
     }
 }
 
